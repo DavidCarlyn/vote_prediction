6aeefc6b604925b28b2bd7af096df29726d501ad
Webs9000 <54490143+Webs9000@users.noreply.github.com>
2020-10-26 22:33:12 -0400

    My code to parse the audio features
    Doesn't seem to quite work yet

---
+++ dan/VoiceParamParse.py
@@ -0,0 +1,96 @@
+import parselmouth
+import numpy as np
+import glob
+import os.path
+
+# Choose which year to run this for
+year = "2015"
+yearFolderPath = "C:/Users/Webs/Documents/CSE/Machine Learn/Project/" + year + "/"
+
+# Output file info
+outputFile = "C:/Users/Webs/Documents/CSE/Machine Learn/Project/featureOutput/" + year + ".txt"
+np.set_printoptions(linewidth=np.inf)
+
+# Get the list of all the case folder paths.  Have to do this as the case numbers aren't contiguous.
+# Also counting the number of case folders.
+casePaths = []
+for subdir, dirs, _ in os.walk(yearFolderPath):
+    for dir in dirs:
+        casePaths.append(os.path.join(subdir, dir))
+
+# Loop over each case in the specified year
+for casePath in casePaths:
+
+    # Count case files in folder and preallocate a NumPy vector.  Doing this to avoid returning new arrays with .append().
+    # Also declare a list for the feature values
+    num_files = len([f for f in os.listdir(casePath) if os.path.isfile(os.path.join(casePath, f))])
+    num_features = num_files * 26
+    feature_vector = np.zeros(num_features, dtype='float32')
+
+    fileIndex = 0
+
+    # test code
+    fileCount = 0
+    print (casePath)
+
+    # Loop over all the mp3 files in the case folder
+    for mp3 in glob.glob(casePath + "/*.mp3"):
+
+        # These are apparently the 3 objects you need
+        sound = parselmouth.Sound(mp3)
+        pitch = sound.to_pitch()
+        pulses = parselmouth.praat.call([sound, pitch], "To PointProcess (cc)")
+
+        # get voice report and create a list for the features
+        voice_report_str = parselmouth.praat.call([sound, pitch, pulses], "Voice report", 0.0, 0.0, 75, 600, 1.3, 1.6, 0.03, 0.45)
+        VPRsplit = voice_report_str.split()
+        feature_list = []
+    
+        # append all the parsed features into the list.  I counted the strings manually assuming the report would always have the same format.
+        feature_list.append(float(VPRsplit[11]))
+        feature_list.append(float(VPRsplit[15]))
+        feature_list.append(float(VPRsplit[19]))
+        feature_list.append(float(VPRsplit[23]))
+        feature_list.append(float(VPRsplit[27]))
+    
+        feature_list.append(float(VPRsplit[33]))
+        feature_list.append(float(VPRsplit[37]))
+        feature_list.append(float(VPRsplit[40]))
+        feature_list.append(float(VPRsplit[46]))
+    
+        feature_list.append(float(VPRsplit[54].strip('%')))
+        feature_list.append(float(VPRsplit[62]))
+        feature_list.append(float(VPRsplit[67].strip('%')))
+    
+        feature_list.append(float(VPRsplit[76].strip('%')))
+        feature_list.append(float(VPRsplit[80]))
+        feature_list.append(float(VPRsplit[84].strip('%')))
+        feature_list.append(float(VPRsplit[87].strip('%')))
+        feature_list.append(float(VPRsplit[90].strip('%')))
+    
+        feature_list.append(float(VPRsplit[94].strip('%')))
+        feature_list.append(float(VPRsplit[98]))
+        feature_list.append(float(VPRsplit[102].strip('%')))
+        feature_list.append(float(VPRsplit[105].strip('%')))
+        feature_list.append(float(VPRsplit[108].strip('%')))
+        feature_list.append(float(VPRsplit[111].strip('%')))
+    
+        feature_list.append(float(VPRsplit[120]))
+        feature_list.append(float(VPRsplit[124]))
+        feature_list.append(float(VPRsplit[128]))
+
+        # Copy the feature list into the correct cells of the numpy array
+        for i in range(26):
+            feature_vector[i + fileIndex] = feature_list[i]
+
+        # up the index by 26 to find the next section of the array to fill
+        fileIndex = fileIndex + 26
+        
+        # this is test code
+        print ("file " + str(mp3) + " completed!")
+        print (fileCount)
+        fileCount = fileCount + 1
+
+    # finally write the array as one line in the output file.
+    with open(outputFile, 'ab') as f:
+        np.savetxt(f, [feature_vector], delimiter=" ")
