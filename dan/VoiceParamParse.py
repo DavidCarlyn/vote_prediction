d3c4be20ff12fe3f487c28cb306b492f17e6bf1d
Webs9000 <54490143+Webs9000@users.noreply.github.com>
2020-10-29 23:50:22 -0400

    See description
    Compensates for buggy transcript files.  Allocates matrix for max(number audio files, transcript lines).  Tags files that throw undefined.  records missing file numbers, case numbers with buggy transcripts, and any cases with number audio files > transcript lines.
    
    To access data:
    
    with open(file) as f:
        missingList = eval(f.readline())
        x = np.genfromtxt(f)

--- dan/VoiceParamParse.py
+++ dan/VoiceParamParse.py
@@ -1,10 +1,12 @@
 import parselmouth
 import numpy as np
 import os.path
+import glob
+import csv
 
-def str_to_float(str_val):
+def str_to_float(str_val, fileNum):
     if str_val == '--undefined--':
-        print("undefined!")
+        print(f'{fileNum} chucked undefined!')
         return -1.0
     else:
         return float(str_val)
@@ -12,6 +14,7 @@ def str_to_float(str_val):
 # Choose which year to run this for
 year = "2015"
 yearFolderPath = "C:/Users/Webs/Documents/CSE/MachineLearn/Project/" + year + "/"
+transcriptFolderPath = "C:/Users/Webs/Documents/CSE/MachineLearn/Project/transcript/"
 
 # Output file info
 outputPath = "C:/Users/Webs/Documents/CSE/MachineLearn/Project/featureOutput/" + year
@@ -23,20 +26,39 @@ for subdir, dirs, _ in os.walk(yearFolderPath):
     for dir in dirs:
         casePaths.append(os.path.join(subdir, dir))
 
-# Loop over each case in the specified year
+# Loop over each case in the specified year.  I'm recording and broken files or cases where audio file > trans lines
+broken_trans_files = []
+more_audio = []
 for casePath in casePaths:
 
     # Count case files in folder and preallocate a NumPy vector.  Doing this to avoid returning new arrays with .append().
     # Also declare a list for the feature values
-    num_files = len([f for f in os.listdir(casePath) if os.path.isfile(os.path.join(casePath, f))])
-    feature_matrix = np.zeros([num_files, 26], dtype='float32')
+    num_audio_files = len([f for f in os.listdir(casePath) if os.path.isfile(os.path.join(casePath, f))])
+
+    # This part is all for counting the lines in the transcript
+    lines = -1
+    case_number = casePath[-5:]
+    transcriptPath = glob.glob(transcriptFolderPath + "*" + case_number + ".csv")[0]
+    try:
+        file = open(transcriptPath)
+        reader = csv.reader(file)
+        lines = len(list(reader))
+    except:
+        broken_trans_files.append(case_number)
+
+    if (0 < lines < num_audio_files):
+        more_audio.append(case_number)
+
+    matrix_rows = max(num_audio_files, lines)
+    feature_matrix = np.zeros([matrix_rows, 26], dtype='float32')
 
     fileIndex = 0
     print (casePath)
 
     # Loop over all the cases in the specified year.  The + 20 is to compensate for any missing files.
     # This is problemetic, as there might be more than 20 missing files from a case.  Come back to this.
-    for file_number in range(num_files + 20):
+    missingFiles = []
+    for file_number in range(matrix_rows):
         
         filePath = casePath + "/" + casePath[-10:] + "_" + str(file_number) + ".mp3"
         
@@ -45,6 +67,7 @@ for casePath in casePaths:
             sound = parselmouth.Sound(filePath)
         except:
             print(f'file {filePath} is missing!')
+            missingFiles.append(file_number)
             continue
 
         pitch = sound.to_pitch()
@@ -54,37 +77,37 @@ for casePath in casePaths:
         VPRsplit = voice_report_str.split()
         feature_list = []
     
-        feature_list.append(str_to_float(VPRsplit[11]))
-        feature_list.append(str_to_float(VPRsplit[15]))
-        feature_list.append(str_to_float(VPRsplit[19]))
-        feature_list.append(str_to_float(VPRsplit[23]))
-        feature_list.append(str_to_float(VPRsplit[27]))
+        feature_list.append(str_to_float(VPRsplit[11], file_number))
+        feature_list.append(str_to_float(VPRsplit[15], file_number))
+        feature_list.append(str_to_float(VPRsplit[19], file_number))
+        feature_list.append(str_to_float(VPRsplit[23], file_number))
+        feature_list.append(str_to_float(VPRsplit[27], file_number))
     
-        feature_list.append(str_to_float(VPRsplit[33]))
-        feature_list.append(str_to_float(VPRsplit[37]))
-        feature_list.append(str_to_float(VPRsplit[40]))
-        feature_list.append(str_to_float(VPRsplit[46]))
+        feature_list.append(str_to_float(VPRsplit[33], file_number))
+        feature_list.append(str_to_float(VPRsplit[37], file_number))
+        feature_list.append(str_to_float(VPRsplit[40], file_number))
+        feature_list.append(str_to_float(VPRsplit[46], file_number))
     
-        feature_list.append(str_to_float(VPRsplit[54].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[62]))
-        feature_list.append(str_to_float(VPRsplit[67].strip('%')) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[54].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[62], file_number))
+        feature_list.append(str_to_float(VPRsplit[67].strip('%'), file_number) / 100.0)
     
-        feature_list.append(str_to_float(VPRsplit[76].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[80]))
-        feature_list.append(str_to_float(VPRsplit[84].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[87].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[90].strip('%')) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[76].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[80], file_number))
+        feature_list.append(str_to_float(VPRsplit[84].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[87].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[90].strip('%'), file_number) / 100.0)
     
-        feature_list.append(str_to_float(VPRsplit[94].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[98]))
-        feature_list.append(str_to_float(VPRsplit[102].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[105].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[108].strip('%')) / 100.0)
-        feature_list.append(str_to_float(VPRsplit[111].strip('%')) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[94].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[98], file_number))
+        feature_list.append(str_to_float(VPRsplit[102].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[105].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[108].strip('%'), file_number) / 100.0)
+        feature_list.append(str_to_float(VPRsplit[111].strip('%'), file_number) / 100.0)
     
-        feature_list.append(str_to_float(VPRsplit[120]))
-        feature_list.append(str_to_float(VPRsplit[124]))
-        feature_list.append(str_to_float(VPRsplit[128]))
+        feature_list.append(str_to_float(VPRsplit[120], file_number))
+        feature_list.append(str_to_float(VPRsplit[124], file_number))
+        feature_list.append(str_to_float(VPRsplit[128], file_number))
 
         # Copy the feature list into the correct cells of the numpy array
         for i in range(26):
@@ -95,7 +118,20 @@ for casePath in casePaths:
         #print (f'file {filePath} completed!')
 
     outputFile = outputPath + "/" + casePath[-10:] + ".txt"
-    np.savetxt(outputFile, feature_matrix)
+    with open(outputFile, "a") as f:
+        f.write("%s\n" % missingFiles)
+        np.savetxt(f, feature_matrix)
+
+    f.close()
     
     print (casePath + " completed!")
-    break
+
+broken_trans_outfile = "C:/Users/Webs/Documents/CSE/MachineLearn/Project/featureOutput/2015/brokenTransFiles.txt"
+btf = open(broken_trans_outfile, "a")
+btf.write("%s" % broken_trans_files)
+
+moreAudioOut = "C:/Users/Webs/Documents/CSE/MachineLearn/Project/featureOutput/2015/moreAudio.txt"
+mao = open(moreAudioOut, "a")
+mao.write("%s" % more_audio)
+btf.close()
+mao.close()
