480fadef7f02905f24a4d8cf704989457d3d3157
seoeun-yang <72570158+seoeun-yang@users.noreply.github.com>
2020-12-07 14:39:24 -0500

    Update model.py

--- seoeun/model.py
+++ seoeun/model.py
@@ -1,3 +1,4 @@
+# import all necessary packages
 import numpy as np
 import pandas as pd
 from keras import Sequential
@@ -16,8 +17,8 @@ from sklearn.metrics import roc_curve, auc
 from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
 
-#load dataset
-pkl_file = open('../data/datas_nor.pkl', 'rb')  
+#load dataset #(written by Seo Eun)
+pkl_file = open('../datas_nor.pkl', 'rb')  
 datas_nor = pickle.load(pkl_file) #normalized version
 pkl_file.close()
 
@@ -25,7 +26,12 @@ pkl_file.close()
 #datas = pickle.load(pkl_file) #without normalization
 #pkl_file.close()
 
-def calc_test_result(result, true_label):
+# a function of presenting prediction outputs from model
+# written by Seo Eun and David
+def calc_test_result(result, true_label): 
+    # input: (1) result: predicted one-hot coded labels
+    #        (2) true_label: true one-hot coded labels
+    # output: print out Confucion Matrix, Classification Report, Accuracy rate, and AUC
     preds = np.argmax(result, axis=1)
     gt = np.argmax(true_label, axis=1)
     print("Confusion Matrix :")
@@ -36,8 +42,14 @@ def calc_test_result(result, true_label):
     tpr, fpr, _ = roc_curve(gt, 1 - result[:, 0], pos_label=0)
     print(f"AUC: {auc(fpr, tpr)}")
 
-
-def build_audio_model(output_dim, use_time_distribution=True, last_activation="relu", return_sequences=True):
+# a function of making a structure of bidirectional LSTM model based on audio data before compile it.
+# written by David and Seo Eun
+def build_audio_model(output_dim, use_time_distribution=True, last_activation="relu", return_sequences=True): 
+    # input: (1) output_dim: the number of units in output layer
+    #        (2) use_time_distribution: determine whether a model adds LSTM layers that return sequences rather than single values
+    #        (3) last_activation: determine an activation function of the last layer
+    #        (4) return_sequences: determine whether LSTM layers return sequences
+    # output: (1) Audio_model: the structure of bi-LSTM model
     Audio_model = Sequential()
     Audio_model.add(Masking(mask_value =0,name='mask_audio'))
     Audio_model.add(Bidirectional(LSTM(20, activation='tanh', return_sequences = return_sequences, name='Bi-LSTM_audio')))
@@ -49,7 +61,14 @@ def build_audio_model(output_dim, use_time_distribution=True, last_activation="r
 
     return Audio_model
 
-def build_text_model(output_dim, use_time_distribution=True, last_activation="relu", return_sequences=True):
+# a function of making a structure of bidirectional LSTM model based on text data before compile it.
+# written by David and Seo Eun
+def build_text_model(output_dim, use_time_distribution=True, last_activation="relu", return_sequences=True):  
+    # input: (1) output_dim: the number of units in output layer
+    #        (2) use_time_distribution: determine whether a model adds LSTM layers that return sequences rather than single values
+    #        (3) last_activation: determine an activation function of the last layer
+    #        (4) return_sequences: determine whether LSTM layers return sequences
+    # output: (1) Text_model: the structure of bi-LSTM model	
     Text_model = Sequential()
     Text_model.add(Masking(mask_value =0,name='mask_text'))
     Text_model.add(Bidirectional(LSTM(125, activation='tanh', return_sequences = return_sequences, name='Bi-LSTM_text')))
@@ -61,11 +80,17 @@ def build_text_model(output_dim, use_time_distribution=True, last_activation="re
 
     return Text_model
 
+# a function of unimodal bidirectional LSTM model (contruct a model, complie, fitting, and then evaluate)
+# written by Seo Eun and David 
 def unimodel(datas, mode, norm,nepoch, batch_size=5):
-
-    ################################################ (written by Seo Eun)
-    # load data
-    # load data
+    # input: (1) datas: training and test dataset (should be a dictionary)
+    #        (2) mode: determine if unimodal is text-based ('text') or audio-based ('audio')
+    #        (3) norm: determine if dataset is normalized or not
+    #        (4) nepoch: the number of epochs
+    #.       (5) batch_size: determine the size of batch (default=5)
+    # output: print classification performances from training and test dataset, and save a trained model.
+    ################################################  
+    # load data 
     train_audio_data=datas['train_audio_data'].astype(np.float32)
     train_text_data=datas['train_text_data'].astype(np.float32)
     test_audio_data=datas['test_audio_data'].astype(np.float32)
@@ -76,64 +101,70 @@ def unimodel(datas, mode, norm,nepoch, batch_size=5):
     train_label = np.unique(datas['train_label'], axis=1).reshape(-1, 2)
     train_data = np.concatenate((train_audio_data,train_text_data), axis=2)
     test_data = np.concatenate((test_audio_data,test_text_data), axis=2)
-
     class_0_weight = 1.0 - ((train_label[:, 0] == 1).sum() / train_label.shape[0])
     class_1_weight = 1.0 - ((train_label[:, 1] == 1).sum() / train_label.shape[0])
     
-    ################################################ (written by Seo Eun)
+    ################################################  
     if mode == 'audio':
         #audio
-        in_audio = Input(shape=(train_audio_data.shape[1],train_audio_data.shape[2]),name='audio_input')
-        Audio_model = build_audio_model(2, False, 'sigmoid',False)
-        Audio_output = Audio_model(in_audio)
+        in_audio = Input(shape=(train_audio_data.shape[1],train_audio_data.shape[2]),name='audio_input') #setting input 
+        Audio_model = build_audio_model(2, False, 'sigmoid',False) #construct audio model
+        Audio_output = Audio_model(in_audio) #its output
     
-        model = Model(in_audio, Audio_output)
-        scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_audio_data.shape[0] // batch_size), decay_rate=0.9)
-        opt = Adam(learning_rate=scheduler)
-        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-        history = model.fit(train_audio_data, train_label,
+        model = Model(in_audio, Audio_output) #make a model
+        scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_audio_data.shape[0] // batch_size), decay_rate=0.9) #learning rate with exponential decay
+        opt = Adam(learning_rate=scheduler) #select optimizer
+        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy']) #complie it
+        history = model.fit(train_audio_data, train_label, #fitting model with 0.2 validation data from training data
         	                epochs=nepoch,
         	                batch_size=batch_size, 
         	                shuffle=True,
-                            class_weight={ 0: class_0_weight, 1: class_1_weight },
+                                class_weight={ 0: class_0_weight, 1: class_1_weight },
         	                validation_split=0.2)
-        model.save(mode+'_'+norm+'.h5') 
-        predicted_train = model.predict(train_audio_data) #709*275*2
-        predicted_test = model.predict(test_audio_data) #177*275*26
+        model.save(mode+'_'+norm+'.h5') #save the trained model
+        predicted_train = model.predict(train_audio_data) #prediction results of training data(Dim: 709*275*2)
+        predicted_test = model.predict(test_audio_data) #prediction results of test data (Dim: 177*275*26)
         
     if mode == 'text':
         #text
-        in_text = Input(shape=(train_text_data.shape[1],train_text_data.shape[2]),name='text_input')
-        Text_model = build_text_model(2, False, 'sigmoid',False)
-        Text_output = Text_model(in_text)
-        model = Model(in_text, Text_output)
-        scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_text_data.shape[0] // batch_size), decay_rate=0.9)
-        opt = Adam(learning_rate=scheduler)
-        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-        history = model.fit(train_text_data, train_label,
-    	                    epochs=nepoch,
+        in_text = Input(shape=(train_text_data.shape[1],train_text_data.shape[2]),name='text_input') #setting input 
+        Text_model = build_text_model(2, False, 'sigmoid',False) #construct audio model
+        Text_output = Text_model(in_text) #its output
+        model = Model(in_text, Text_output) #make a model
+        scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_text_data.shape[0] // batch_size), decay_rate=0.9) #learning rate with exponential decay
+        opt = Adam(learning_rate=scheduler) #select optimizer
+        model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy']) #complie it
+        history = model.fit(train_text_data, train_label, #fitting model with 0.2 validation data from training data
+    	                    epochs=nepoch, 
     	                    batch_size=batch_size,
     	                    shuffle=True,
                             class_weight={ 0: class_0_weight, 1: class_1_weight },
     	                    validation_split=0.2)
-        model.save(mode+'.h5') 
-        predicted_train = model.predict(train_text_data) #709*275*2
-        predicted_test = model.predict(test_text_data)
+        model.save(mode+'.h5') #save the trained model
+        predicted_train = model.predict(train_text_data) #prediction results of training data(Dim: #709*275*2)
+        predicted_test = model.predict(test_text_data) #prediction results of test data (Dim: 177*275*26)
     
-    # predict
+    # show predicted outcomes
     print('-----train result-----')
     calc_test_result(predicted_train, train_label)
     print('-----test result-----')
     calc_test_result(predicted_test, test_label)
     
-    # save model
+    # save the final model
     output = open('result_'+mode+'.pkl', 'wb')  
     pickle.dump({'pre_train':predicted_train,'pre_test':predicted_test}, output)
     output.close()     
 
+# a function of multimodal bidirectional LSTM model (contruct a model, complie, fitting, and then evaluate)
+# written by Seo Eun, David and Dan
 def multimodel(datas, mode, nepoch, batch_size=5):  
+    # input: (1) datas: training and test dataset (should be a dictionary)
+    #        (2) mode: determine if unimodal is text-based ('text') or audio-based ('audio') 
+    #        (3) nepoch: the number of epochs
+    #.       (4) batch_size: determine the size of batch (default=5)
+    # output: print classification performances from training and test dataset, and save a trained model.
     
-    ################################################ (written by Seo Eun)
+    ################################################  
     # load data
     train_audio_data=datas['train_audio_data'].astype(np.float32)
     train_text_data=datas['train_text_data'].astype(np.float32)
@@ -144,8 +175,7 @@ def multimodel(datas, mode, nepoch, batch_size=5):
     test_label = np.unique(datas['test_label'], axis=1).reshape(-1, 2)
     train_label = np.unique(datas['train_label'], axis=1).reshape(-1, 2)
     train_data = np.concatenate((train_audio_data,train_text_data), axis=2)
-    test_data = np.concatenate((test_audio_data,test_text_data), axis=2)
-    
+    test_data = np.concatenate((test_audio_data,test_text_data), axis=2) 
     class_0_weight = 1.0 - ((train_label[:, 0] == 1).sum() / train_label.shape[0])
     class_1_weight = 1.0 - ((train_label[:, 1] == 1).sum() / train_label.shape[0])
 
@@ -154,40 +184,39 @@ def multimodel(datas, mode, nepoch, batch_size=5):
     #audio
     in_audio = Input(shape=(train_audio_data.shape[1],train_audio_data.shape[2]),name='audio_input')
     Audio_model = build_audio_model(1)
-    Audio_output = Audio_model(in_audio)
+    Audio_output = Audio_model(in_audio) 
     #text
     in_text = Input(shape=(train_text_data.shape[1],train_text_data.shape[2]),name='text_input')
     Text_model = build_text_model(1)   
     Text_output = Text_model(in_text)
 
-    #merging audio and text
+    #merging audio and text from two outputs from audio and text
     merged = Concatenate(axis=2)([Audio_output,Text_output])
     	 
-    ################################################ (written by Dan and Seo Eun and David)
+    ################################################ (written by Dan, Seo Eun and David)
     #stage 2: BiLSTM with a two-layer neural network 
     Combined_model = Sequential() 
-    Combined_model.add(Bidirectional(LSTM(150, activation='tanh', dropout=0.5, name='Bi-LSTM_merged')))
-    Combined_model.add(Dropout(0.5, name='Dropout_com1'))
-    #Combined_model.add(Dense(100, activation='relu', name='fc_merged1'))
-    #Combined_model.add(Dropout(0.5, name='Dropout_com2'))
-    Combined_model.add(Dense(2, activation='sigmoid', name='output')) 
-    output = Combined_model(merged)
+    Combined_model.add(Bidirectional(LSTM(150, activation='tanh', dropout=0.5, name='Bi-LSTM_merged'))) #bi-LSTM model
+    Combined_model.add(Dropout(0.5, name='Dropout_com1')) #dropout 0.5 rate
+    Combined_model.add(Dense(2, activation='sigmoid', name='output')) #output layer
+    output = Combined_model(merged) 
     #
     ################################################ (written by Seo Eun)
-    model = Model([in_audio,in_text], output)
-    #scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_audio_data.shape[0] // batch_size), decay_rate=0.9)
-    opt = Adam(learning_rate=0.0003)
-    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-    history = model.fit([train_audio_data, train_text_data], train_label,
+    model = Model([in_audio,in_text], output) #model
+    scheduler = ExponentialDecay(initial_learning_rate=0.0003, decay_steps=(nepoch // 10) * (train_audio_data.shape[0] // batch_size), decay_rate=0.9) #learning rate with exponential Decay
+    opt = Adam(learning_rate=scheduler) #use Adam optimizer
+    model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy']) #compile a model
+    history = model.fit([train_audio_data, train_text_data], train_label, #fitting a model
     	                epochs=nepoch,
     	                batch_size=batch_size, 
     	                shuffle=True,
-                        #class_weight={ 0: class_0_weight, 1: class_1_weight },
+                        class_weight={ 0: class_0_weight, 1: class_1_weight },
     	                validation_split=0.2)
-    model.save(mode+'.h5') 
-    predicted_train = model.predict([train_audio_data,train_text_data]) #709*275*2
-    predicted_test = model.predict([test_audio_data,test_text_data])
+    model.save(mode+'.h5') #save the trained model
+    predicted_train = model.predict([train_audio_data,train_text_data]) #prediction results of training data(Dim: #709*2)
+    predicted_test = model.predict([test_audio_data,test_text_data]) #prediction results of test data(Dim: #177*2)
 
+    # show predicted outcomes
     print('-----train result-----')
     calc_test_result(predicted_train, train_label)
     print('-----test result-----')
@@ -210,12 +239,13 @@ def multimodel(datas, mode, nepoch, batch_size=5):
     plt.legend(['train', 'validation'], loc='upper left')
     plt.show()    
     
+    # save the trained model
     output = open('result_'+mode+'.pkl', 'wb')  
     pickle.dump({'pre_train':predicted_train,'pre_test':predicted_test}, output)
     output.close() 
     
 if __name__=="__main__":
-	
+
     print('----- multimodal -----')
     #multimodel(datas,'multimodal_no_normalized',50)
     multimodel(datas_nor,'multimodal_normalized', 50, 5)
